373a4d7bf38850dc0e663f65b1af4956d7408cd9
Alejandro Brozzo <alejandro.brozzo@wunderdog.fi>
2025-02-23 17:36:00 +0100

    Add pencil marks and cell container for Sudoku grid

--- src/components/SudokuCell.tsx
+++ src/components/SudokuCell.tsx
@@ -16,5 +16,22 @@ export const SudokuCell = ({ value, onChange }: SudokuCellProps) => {
     }
   }
 
-  return <input type="number" min="1" max="9" value={value || ''} onChange={handleChange} className="sudoku-cell" />
+  return (
+    <div className="cell-container">
+      {value ? (
+        <input type="number" min="1" max="9" value={value} onChange={handleChange} className="sudoku-cell" />
+      ) : (
+        <>
+          <input type="number" min="1" max="9" value="" onChange={handleChange} className="sudoku-cell" />
+          <div className="pencil-marks">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
+              <span key={num} className={`pencil-mark pos-${num}`}>
+                {num}
+              </span>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
